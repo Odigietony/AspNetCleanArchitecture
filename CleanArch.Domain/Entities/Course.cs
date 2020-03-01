d814ae6df546fcd1e6a41a793d0283c526428be5
Odigietony <odigietony.jr@gmail.com>
2020-03-01 16:27:04 +0100

    Add project files.

---
+++ CleanArch.Domain/Entities/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CourseCode { get; set; }
+    }
+}
