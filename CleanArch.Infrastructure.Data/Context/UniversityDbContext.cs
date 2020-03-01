d737db56578f0e3e120c1a838af84fa8b7460e19
Odigietony <odigietony.jr@gmail.com>
2020-03-01 19:01:22 +0100

    Data context

---
+++ CleanArch.Infrastructure.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infrastructure.Data.Context
+{
+    class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Course> Courses { get; set; }
+    }
+}
