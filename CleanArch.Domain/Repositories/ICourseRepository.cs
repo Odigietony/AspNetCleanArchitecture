cf163bf015f209712c0a00201be7cf7c2554f04d
Odigietony <odigietony.jr@gmail.com>
2020-03-06 10:21:05 +0100

    Commit course repository from the Domain layer

---
+++ CleanArch.Domain/Repositories/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Repositories
+{
+    public interface ICourseRepository
+    {
+        public IEnumerable<Course> GetCourses();
+    }
+}
