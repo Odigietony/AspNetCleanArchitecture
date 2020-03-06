ac550120b29dc992f9e39d76d091d8c560a5b22b
Odigietony <odigietony.jr@gmail.com>
2020-03-06 11:43:08 +0100

    Configure Presentation layer for Dependency container in the infrastructure inversion of control layer

--- CleanArch.MVC/Startup.cs
+++ CleanArch.MVC/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CleanArch.Infrastructure.Data.Context;
+using CleanArch.Infra.InversionOfControl;
 
 namespace CleanArch.MVC
 {
@@ -39,6 +40,7 @@ namespace CleanArch.MVC
             });
             services.AddControllersWithViews();
             services.AddRazorPages();
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,6 +72,11 @@ namespace CleanArch.MVC
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
