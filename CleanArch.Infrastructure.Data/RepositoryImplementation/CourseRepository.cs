683cdc3c3b88840a700963f6c9acd491142f669b
Odigietony <odigietony.jr@gmail.com>
2020-03-06 11:03:57 +0100

    Commit Service and repository interfaces in the application and data layers respectively

---
+++ CleanArch.Infrastructure.Data/RepositoryImplementation/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Repositories;
+using CleanArch.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infrastructure.Data.RepositoryImplementation
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
