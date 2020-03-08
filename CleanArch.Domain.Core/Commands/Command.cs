c6898921eb2810cfb506ce2dd3eab752f4525367
Odigietony <odigietony.jr@gmail.com>
2020-03-08 03:34:56 +0100

    Adding Domain core and Implementing InMemoryBus

---
+++ CleanArch.Domain.Core/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using CleanArch.Domain.Core.Event;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
