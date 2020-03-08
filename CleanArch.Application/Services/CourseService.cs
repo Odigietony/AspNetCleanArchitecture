3e2385f63ba2aa689d29ab35211e48561e802679
Odigietony <odigietony.jr@gmail.com>
2020-03-08 05:27:29 +0100

    Updated Course view model, services and interface from the Application Layer

--- CleanArch.Application/Services/CourseService.cs
+++ CleanArch.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Repositories;
 using System;
 using System.Collections.Generic;
@@ -10,11 +12,20 @@ namespace CleanArch.Application.Services
     public class CourseService : ICourseService
     {
         private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _handler;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler handler)
         {
             _courseRepository = courseRepository;
+            _handler = handler;
         }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            CreateCourseCommand create = new CreateCourseCommand(courseViewModel.Name, courseViewModel.CourseCode);
+            _handler.SendCommand(create);
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel()
